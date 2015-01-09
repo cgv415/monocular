47f60faf88df53be8e50980a49776d9241cc9614
cgv415 <carlosgarrido993@gmail.com>
2015-01-09 22:31:03 +0100

    Modelo Menu Eliminado

---
+++ apps/servicios/migrations/0001_initial.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('administracion', '0002_empleado'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Corto',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=100)),
+                ('palmares', models.TextField()),
+                ('duracion', models.IntegerField(default=b'0')),
+                ('anyo', models.DateField(verbose_name=b'yyyy')),
+                ('pais', models.CharField(max_length=50)),
+                ('director', models.CharField(max_length=50)),
+                ('reparto', models.CharField(max_length=50)),
+                ('productora', models.CharField(max_length=100)),
+                ('genero', models.CharField(max_length=100)),
+                ('sinopsis', models.TextField()),
+                ('trailer', models.CharField(max_length=100, null=True)),
+                ('imagen', models.ImageField(upload_to=b'cortometrajes')),
+                ('cliente', models.OneToOneField(to='administracion.Cliente')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Estado_Corto',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('estado', models.CharField(blank=True, max_length=30, null=True, choices=[(b'seleccionado', b'Seleccionado'), (b'premiado', b'Premiado')])),
+                ('corto', models.OneToOneField(to='servicios.Corto')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Festival',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=100)),
+                ('ciudad', models.CharField(max_length=100)),
+                ('pais', models.CharField(max_length=100)),
+                ('anyo', models.PositiveIntegerField()),
+                ('fecha', models.CharField(max_length=100, null=True, blank=True)),
+                ('web', models.URLField(null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='estado_corto',
+            name='festival',
+            field=models.OneToOneField(to='servicios.Festival'),
+            preserve_default=True,
+        ),
+    ]
--- apps/servicios/views.py
+++ apps/servicios/views.py
@@ -1,31 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from apps.base.models import Menu
 from .models import Corto
 # Create your views here.
 def Servicios(request):
-    menus = Menu.objects.all()
-    return render_to_response('servicios/servicios.html',{'menus':menus},context_instance=RequestContext(request))
+    return render_to_response('servicios/servicios.html',context_instance=RequestContext(request))
 
 def Produccion(request):
-    menus = Menu.objects.all()
-    return render_to_response('servicios/produccion.html',{'menus':menus},context_instance=RequestContext(request))
+    return render_to_response('servicios/produccion.html',context_instance=RequestContext(request))
 
 def Postproduccion(request):
-    menus = Menu.objects.all()
-    return render_to_response('servicios/postproduccion.html',{'menus':menus},context_instance=RequestContext(request))
+    return render_to_response('servicios/postproduccion.html',context_instance=RequestContext(request))
 
 def Publicidad(request):
-    menus = Menu.objects.all()
-    return render_to_response('servicios/publicidad.html',{'menus':menus},context_instance=RequestContext(request))
+    return render_to_response('servicios/publicidad.html',context_instance=RequestContext(request))
 
 def Distribucion(request):
-    menus = Menu.objects.all()
     cortos = Corto.objects.all()
-    return render_to_response('servicios/distribucion.html',{'menus':menus,'cortos':cortos},context_instance=RequestContext(request))
+    return render_to_response('servicios/distribucion.html',{'cortos':cortos},context_instance=RequestContext(request))
 
 def Cortometraje(request,offset):
-    menus = Menu.objects.all()
     cod = int(offset)
     corto = Corto.objects.filter(id=cod)[0]
-    return render_to_response('servicios/cortometraje.html',{'menus':menus,'corto':corto},context_instance=RequestContext(request))
+    return render_to_response('servicios/cortometraje.html',{'corto':corto},context_instance=RequestContext(request))
