ec648ca34db5efefd58e45af1299a50246aa0a47
cgv415 <carlosgarrido993@gmail.com>
2015-01-09 22:31:14 +0100

    Modelo Menu Eliminado

--- apps/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from .views import base
-
-urlpatterns = [
-   #url(r'^$' , 'apps.base.views.principal'),
-   url(r'^/base$' , 'apps.base.views.base'),
-]
