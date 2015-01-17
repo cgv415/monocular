8fadf6c7213a151f7fb79d784cf071d11d6ff2ac
cgv415 <carlosgarrido993@gmail.com>
2015-01-17 20:45:53 +0100

    Primera version mejorada administracion manual

--- apps/administracion/urls.py
+++ apps/administracion/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import Registrarse, RegistrarCorto,RegistrarNoticia,RegistrarFestival,ModificarNoticia
+from .views import RegistrarCorto,RegistrarNoticia,RegistrarFestival,ModificarNoticia
 from django.contrib.auth.views import login
 from apps.administracion.views import ModificarCorto, DeleteNoticia,NoticiasList, ProyectosList
 #from django.contrib.auth.views import logout
@@ -12,8 +12,6 @@ urlpatterns = [
    url(r'^registrarFestival/', RegistrarFestival.as_view(), name="registrar_festival"),
    url(r'^registrarNoticia/', RegistrarNoticia.as_view(), name="registrar_noticia"),
    
-   url(r'^verCortos/', 'apps.administracion.views.VerCortos', name="ver_cortos"),
-   url(r'^verNoticias/', 'apps.administracion.views.VerNoticias', name="ver_noticias"),
    url(r'^verUsuarios/', 'apps.administracion.views.VerUsuarios', name="ver_usuarios"),
    url(r'^verAdministradores/', 'apps.administracion.views.VerAdministradores', name="ver_administradores"),
    url(r'^verFestivales/', 'apps.administracion.views.VerFestivales', name="ver_festivales"),
@@ -25,11 +23,7 @@ urlpatterns = [
    url(r'^modificarCorto/(?P<pk>\d+)/$', ModificarCorto.as_view(), name='modificar_corto'),
    url(r'^eliminarNoticia/(?P<pk>\d+)/$', DeleteNoticia.as_view(), name='eliminar_noticia'),
    
-   url(r'^registrarse/$' , Registrarse.as_view(),name="registrarse"),
-   url(r'^main$', 'apps.administracion.views.main', name='main'),
-   url(r'^signup$', 'apps.administracion.views.signup', name='signup'),
    url(r'^$', login, {'template_name': 'administracion/login.html', }, name="login"),
-   #url(r'^home$', 'apps.administracion.views.home', name='home'),
    url(r'^logout/$' , 'django.contrib.auth.views.logout_then_login',name='logout'),
 
    ]
--- apps/administracion/views.py
+++ apps/administracion/views.py
@@ -1,17 +1,12 @@
-from django.contrib.auth.decorators import login_required
-from django.http.response import HttpResponseRedirect
-from django.views.generic import CreateView,FormView
+#from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render_to_response
-from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.template import RequestContext
 
 from apps.servicios.models import Proyecto, Festival
 from apps.noticias.models import Noticia
-from .forms import UserForm
-from .models import Cliente
-from forms import SignUpForm
 from django.views.generic.edit import UpdateView, DeleteView
 from django.views.generic.list import ListView
 
@@ -21,77 +16,8 @@ from django.views.generic.list import ListView
 def Principal(request):
     return render_to_response('administracion/admin.html',context_instance=RequestContext(request))
 
-#Sin uso
-def main(request):
-    return render_to_response('administracion/main.html', context_instance=RequestContext(request))
-
-#Sin uso
-@login_required()
-def home(request):
-    usuario = request.user
-    if usuario.is_staff:
-        return render_to_response('administracion/admin.html', {'user': request.user}, context_instance=RequestContext(request)) 
-    else:
-        return render_to_response('administracion/admin.html', {'user': request.user}, context_instance=RequestContext(request)) 
-    
-#Sin uso       
-def signup(request):
-    if request.method == 'POST':  # If the form has been submitted...
-        form = SignUpForm(request.POST)  # A form bound to the POST data
-        if form.is_valid():  # All validation rules pass
- 
-            # Process the data in form.cleaned_data
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            email = form.cleaned_data["email"]
-            first_name = form.cleaned_data["first_name"]
-            last_name = form.cleaned_data["last_name"]
- 
-            # At this point, user is a User object that has already been saved
-            # to the database. You can continue to change its attributes
-            # if you want to change other fields.
-            user = User.objects.create_user(username, email, password)
-            user.first_name = first_name
-            user.last_name = last_name
- 
-            # Save new user attributes
-            user.save()
-            
-            #Creating Perfil
-            cliente = Cliente()
-            cliente.usuario = user
-            cliente.telefono = form.cleaned_data['telefono']
-            cliente.servicio_Contratado = form.cleaned_data['servicio_Contratado']
-            cliente.proyecto = form.cleaned_data['proyecto']
-            cliente.save()
- 
-            return HttpResponseRedirect(reverse('main'))  # Redirect after POST
-    else:
-        form = SignUpForm()
- 
-    data = {
-        'form': form,
-    }
-    return render_to_response('administracion/signup.html', data, context_instance=RequestContext(request))
-
-#Sin uso
-class Registrarse(FormView):
-    template_name='administracion/signup.html'
-    form_class=UserForm
-    success_url = reverse_lazy('registrarse')
-    
-    def form_valid(self,form):
-        user = form.save()
-        cliente = Cliente()
-        cliente.usuario = user
-        cliente.telefono = form.cleaned_data['telefono']
-        cliente.servicio_Contratado = form.cleaned_data['servicio_Contratado']
-        cliente.proyecto = form.cleaned_data['proyecto']
-        cliente.save()
-        return super(Registrarse,self).form_valid(form)
 
 #Comienzan las vistas para crear objetos con create view
-
 class RegistrarCorto(CreateView):
     template_name='administracion/registrar.html'
     model = Proyecto
@@ -101,17 +27,16 @@ class RegistrarCorto(CreateView):
 class RegistrarNoticia(CreateView):
     template_name='administracion/registrar.html'
     model = Noticia
-    fields=['titulo','resumen','texto','fecha','imagen']
-    success_url = reverse_lazy('/administracion/')
+    fields=['titulo','texto','resumen','fecha','imagen']
+    success_url = '/administracion/noticiaslist'
 
 class RegistrarFestival(CreateView):
     template_name='administracion/registrar.html'
     model = Festival
     fields=['nombre','ciudad','pais','anyo','fecha','web']
-    success_url = '/administracion/'
+    success_url = '/administracion/verFestivales'
 
 #Comienzan las vistas para modificar objetos con update view
-
 class ModificarNoticia(UpdateView):
     model = Noticia
     fields=['titulo','resumen','texto','fecha','imagen']
@@ -125,18 +50,6 @@ class ModificarCorto(UpdateView):
     template_name_suffix = '_update_form'
     success_url = '/inicio/'
     
-#Comienzan las vistas para ver los objetos, lo ideal seria con listview pero ya esta hecho y no hay tiempo
-
-#En desuso
-def VerCortos(request):
-    cortos = Proyecto.objects.all().order_by('-id')
-    return render_to_response('administracion/verCortos.html',{'cortos': cortos},context_instance=RequestContext(request))
-
-#En desuso
-def VerNoticias(request):
-    noticias = Noticia.objects.all().order_by('-id')
-    return render_to_response('administracion/verNoticias.html',{'noticias': noticias},context_instance=RequestContext(request))
-
 #Vista de los objetos con listview
 class NoticiasList(ListView):
     model = Noticia
@@ -144,6 +57,7 @@ class NoticiasList(ListView):
 class ProyectosList(ListView):
     model = Proyecto
 
+#Cambiar
 def VerUsuarios(request):
     usuarios = User.objects.all().filter(is_staff=False)
     return render_to_response('administracion/verUsuarios.html',{'usuarios': usuarios},context_instance=RequestContext(request))
@@ -160,4 +74,4 @@ def VerFestivales(request):
 #Comienzan las vistas para eliminar objetos de la base de datos con delete view
 class DeleteNoticia(DeleteView):
     model = Noticia
-    success_url = reverse_lazy('inicio')
+    success_url = '/administracion/noticiaslist'
