e40af16e96d0891778e5a0ce331770da4b4aee95
cgv415 <carlosgarrido993@gmail.com>
2015-01-25 23:22:26 +0100

    Redirigir a admin, no permitir administracion a no staff

--- apps/administracion/views.py
+++ apps/administracion/views.py
@@ -11,6 +11,7 @@ from django.views.generic.edit import UpdateView, DeleteView, FormView
 from django.views.generic.list import ListView
 from apps.administracion.models import Cliente, Empleado
 from apps.administracion.forms import UserForm
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 #Vista principal de la administracion
@@ -28,19 +29,18 @@ def EmpleadoView(request,offset):
     empleado = Empleado.objects.filter(id=cod)[0]
     return render_to_response('administracion/empleado.html',{'empleado':empleado},context_instance=RequestContext(request))
 
-
 class RegistrarUsuario(CreateView):
     template_name = 'administracion/registrar.html'
     fields=['username','password','first_name','last_name','email']
     model = User
     success_url = '/administracion/registrarCliente'
-    
+
 class RegistrarUsuarioEmpleado(CreateView):
     template_name = 'administracion/registrar.html'
     fields=['username','password','first_name','last_name','email']
     model = User
     success_url = '/administracion/registrarAdministrador'
-    
+     
 class AdministradorList(ListView):
     model = Empleado
 
@@ -49,40 +49,42 @@ class RegistrarAdministrador(CreateView):
     model = Empleado
     fields=['usuario',]
     success_url = '/administracion/administradorlist'
-    
+   
 class ModificarAdministrador(UpdateView):
     model = Empleado
     fields=['usuario',]
     template_name='administracion/modificar.html'
     success_url = '/administracion/administradorlist'
-    
+@staff_member_required      
 class DeleteAdministrador(DeleteView):
     template_name='administracion/delete.html'
     model = Empleado
     success_url = '/administracion/administradorlist'    
     
 #Operaciones sobre Clientes
+ 
 class ClientesList(ListView):
     model = Cliente
-    
+   
 class RegistrarCliente(CreateView):
     template_name = 'administracion/registrarCliente.html'
     fields=['usuario','ciudad','telefono']
     model = Cliente
     success_url = '/administracion/clienteslist'
-    
+     
 class ModificarCliente(UpdateView):
     model = Cliente
     fields=['usuario','ciudad','telefono']
     template_name='administracion/modificar.html'
     success_url = '/administracion/clienteslist'
-        
+    
 class DeleteCliente(DeleteView):
     template_name='administracion/delete.html'
     model = Cliente
     success_url = '/administracion/clienteslist'
 
 #Operaciones sobre Proyecto
+
 class ProyectosList(ListView):
     model = Proyecto
     
@@ -91,7 +93,8 @@ class RegistrarCorto(CreateView):
     model = Proyecto
     fields=['cliente','titulo','sinopsis','duracion','anyo','pais','director','reparto','productora','genero','trailer','imagen']
     success_url = '/administracion/proyectoslist'
-    
+
+   
 class ModificarCorto(UpdateView):
     model = Proyecto
     fields=['cliente','titulo','sinopsis','duracion','anyo','pais','director','reparto','productora','genero','trailer','imagen']
@@ -112,11 +115,13 @@ class RegistrarNoticia(CreateView):
     model = Noticia
     fields=['titulo','texto','resumen','fecha','imagen']
     success_url = '/administracion/noticiaslist'
+    
 class ModificarNoticia(UpdateView):
     model = Noticia
     fields=['titulo','resumen','texto','fecha','imagen']
     template_name='administracion/modificar.html'
     success_url = '/administracion/noticiaslist'
+
 class DeleteNoticia(DeleteView):
     template_name='administracion/delete.html'
     model = Noticia
@@ -125,41 +130,46 @@ class DeleteNoticia(DeleteView):
 #Operaciones sobre Festivales
 class FestivalesList(ListView):
     model = Festival
+
 class RegistrarFestival(CreateView):
     template_name='administracion/registrar.html'
     model = Festival
     fields=['nombre','ciudad','pais','anyo','fecha','web']
     success_url = '/administracion/festivaleslist'
+
 class ModificarFestival(UpdateView):
     model = Festival
     fields=['nombre','ciudad','pais','anyo','fecha','web']
     template_name='administracion/modificar.html'
-    success_url = '/administracion/festivaleslist'    
+    success_url = '/administracion/festivaleslist'  
     
 class DeleteFestival(DeleteView):
     template_name='administracion/delete.html'
     model = Festival
     success_url = '/administracion/festivaleslist'  
     
-#Operaciones sobre Estados
+#Operaciones sobre Estados 
 class EstadosList(ListView):
     model = Estado_Proyecto
+
 class RegistrarEstado(CreateView):
     template_name='administracion/registrar.html'
     model = Estado_Proyecto
     fields=['corto','festival','estado']
     success_url = '/administracion/estadoslist'
+
 class ModificarEstado(UpdateView):
     model = Estado_Proyecto
     fields=['corto','festival','estado']
     template_name='administracion/modificar.html'
     success_url = '/administracion/estadoslist'
+
 class DeleteEstado(DeleteView):
     template_name='administracion/delete.html'
     model = Estado_Proyecto
     success_url = '/administracion/estadoslist'   
 
-#Operaciones sobre Estados
+#Operaciones sobre Estados  
 class PublicidadDetails(ListView):
     model = Publicidad
 
@@ -180,6 +190,9 @@ class DeleteAnuncio(DeleteView):
     model = Publicidad
     fields=['titulo','imagen','video']
     success_url = '/administracion/publicidadDetails'
+
+def Avanzado(request):
+    return render_to_response('/admin/',context_instance=RequestContext(request))
 
 #Cambiar
 def VerUsuarios(request):
--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
    #url(r'^$' , 'apps.inicio.views.index'),
-   url(r'^$' , 'apps.inicio.views.inicio'),
+   url(r'^$' , 'apps.inicio.views.inicio',name="inicio"),
    url(r'^inicio' , 'apps.inicio.views.inicio'),
    url(r'^contacto' , 'apps.inicio.views.contacto',name="contacto"),
    ]
